2f0caee6f3fa64626d3fc7601250b791b417034e
QiankunH1 <1821330332@qq.com>
2019-04-28 17:00:59 +0800

    index页面

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    animated();
+    tab() ;
+
+});
+function tab() {
+    $(".list_3 .item_l li").click(function () {
+        //获取点击的元素给其添加样式，讲其兄弟元素的样式移除
+        var index = $(this).index();
+        $(".list_3 .item_l .text").removeClass("active").eq(index).addClass("active");
+        //获取选中元素的下标
+        $(".list_3 .item_r").removeClass("active").eq(index).addClass("active")
+    });
+};
+function animated(){
     var a, b, c;
     a = $(window).height();
     $(window).scroll(function () {
@@ -33,6 +47,16 @@ $(document).ready(function () {
                 $(this).removeClass("rotateIn");
             }
         });
+        $(".list_3 .item_r").each(function () {
+            c = $(this).offset().top;
+            if (a + b > c) {
+                $(this).addClass("animated");
+                $(this).addClass("slideInRight");
+            }
+            else {
+                $(this).removeClass("slideInRight");
+            }
+        });
         $(".list_4 .kh_bg").each(function () {
             c = $(this).offset().top;
             if (a + b > c) {
@@ -54,5 +78,4 @@ $(document).ready(function () {
             }
         });
     });
-
-});
+}
